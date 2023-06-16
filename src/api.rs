b95c6744689435af9ba9f1d6a14fd2fab3bdcc9a
ThatBoiDev <mart.zielman@kpnmail.nl>
2023-06-16 17:37:36 +0200

    📊 Added SQL

--- src/api.rs
+++ src/api.rs
@@ -1,3 +1,36 @@
-// TODO: Save the files in a database
+use rusqlite::{Connection, Result};
+use crate::Note;
 
 
+
+pub fn save_note(note: &Note) -> Result<()> {
+    let sqlite = Connection::open("notes.db")?;
+
+    sqlite.execute(
+            "INSERT INTO saved_notes (name, content, created) VALUES (?1, ?2, ?3);",
+            (&note.name, &note.content, &note.created),
+        )?;
+
+    Ok(())
+}
+
+pub fn get_notes() -> Result<Vec<Note>> {
+    let sqlite = Connection::open("notes.db")?;
+
+    let mut stmt = sqlite.prepare("SELECT name, content, created FROM saved_notes;")?;
+    let note_iter = stmt.query_map((), |row| {
+        Ok(Note {
+            name: row.get(0)?,
+            content: row.get(1)?,
+            created: row.get(2)?,
+        })
+    })?;
+
+    let mut notes: Vec<Note> = Vec::new();
+
+    for note in note_iter {
+        notes.push(note?);
+    }
+
+    Ok(notes)
+}
--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,21 @@ use chrono::prelude::*;
 pub struct Note {
     name: String,
     content: String,
-    created: DateTime<Local>
-}
-
-impl Note {
-    fn save_note(&self, saved_notes: &mut Vec<Note>) {
-        // The API for storing the notes is not yet implemented, for now this is fine:
-        saved_notes.push(Note {
-            name: self.name.clone(),
-            content: self.content.clone(),
-            created: self.created,
-        });
-        println!("Your note is saved.");
-        show_notes(saved_notes);
-    }
+    created: String
 }
 
 fn main() {
-    let mut saved_notes: Vec<Note> = Vec::new();
+    // api::init_db().expect("Initializing failed.");
 
     loop {
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        show_notes(&saved_notes);
+        show_notes();
 
         let new_note_bool = prompt_default("Do you want to create a new note?", false);
 
         match new_note_bool {
             Ok(true) => {
-                new_note(&mut saved_notes).expect("");
+                new_note().expect("");
                 print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
             },
             Ok(false) => {
@@ -48,7 +35,10 @@ fn main() {
 }
 
 
-fn show_notes(saved_notes: &[Note]) {
+fn show_notes() {
+    let mut saved_notes: Vec<Note> = Vec::new();
+    saved_notes = api::get_notes().expect("");
+
     println!("Welcome to Notabena, your favorite note taking app.");
     println!("=======================");
     if saved_notes.is_empty() {
@@ -58,17 +48,17 @@ fn show_notes(saved_notes: &[Note]) {
         for note in saved_notes {
             println!("{}", note.name);
             println!("{}", note.content);
-            println!("Created at: {:02}:{:02}", note.created.hour(), note.created.minute());
+            println!("Created at: {}", note.created);
             println!("=======================");
         }
     };
 }
 
-fn new_note(saved_notes: &mut Vec<Note>) -> Result<(), Box<dyn std::error::Error>> {
+fn new_note() -> Result<(), Box<dyn std::error::Error>> {
     let inputted_note = Note {
         name: prompt("Name")?,
         content: prompt("Content")?,
-        created: Local::now()
+        created: Local::now().hour().to_string() + ":" + &Local::now().minute().to_string(),
     };
 
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
@@ -76,14 +66,14 @@ fn new_note(saved_notes: &mut Vec<Note>) -> Result<(), Box<dyn std::error::Error
     println!("=======================");
     println!("Name: {}", inputted_note.name);
     println!("Content: {}", inputted_note.content);
-    println!("Created at: {:02}:{:02}", inputted_note.created.hour(), inputted_note.created.minute());
+    println!("Created at: {}", inputted_note.created);
     println!("=======================");
 
     let save_note_bool = prompt_default("Do you want to save this note?", true);
 
     return match save_note_bool {
         Ok(true) => {
-            inputted_note.save_note(saved_notes);
+            api::save_note(&inputted_note)?;
             Ok(())
         },
         Ok(false) => {
