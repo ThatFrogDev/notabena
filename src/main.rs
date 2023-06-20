95cdc513785080972abe70916ef8189c07513849
MrSerge01 <olgadeeva61@gmail.com>
2023-06-20 17:08:00 +0600

    simplified the code a little bit + WIP multidelete

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 pub mod api;
 
 use chrono::prelude::*;
-use inquire::{Confirm, Select, Text};
+use inquire::{Confirm, Select, MultiSelect, Text};
 
 pub struct Note {
     id: usize,
@@ -18,7 +18,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         cursor_to_origin();
 
-        let options: Vec<&str> = vec!["New note", "View notes", "Edit note", "Delete note", "Exit"];
+        let options: Vec<&str> = vec!["New note", "View note", "Edit note", "Delete note", "Exit"];
         let select = Select::new("What do you want to do?", options).prompt();
 
         match select {
@@ -26,7 +26,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 new_note().expect("Creating a new note failed");
                 cursor_to_origin();
             },
-            Ok("View notes") => {
+            Ok("View note") => {
                 show_notes()?;
                 cursor_to_origin();
             },
@@ -58,12 +58,8 @@ fn new_note() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     cursor_to_origin();
-    println!("This is the note you just created:");
-    println!("=======================");
-    println!("Name: {}", inputted_note.name);
-    println!("Content: {}", inputted_note.content);
-    println!("Created at: {}", inputted_note.created);
-    println!("=======================");
+    println!("This is the note you're about to create:");
+    display_note(&inputted_note);
 
     let save_note_bool = Confirm::new("Do you want to save this note?")
         .with_default(true)
@@ -84,16 +80,15 @@ fn new_note() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn show_notes() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: Make the notes selectable to fully view them
     let saved_notes = api::get_notes()?;
+    let mut options: Vec<String> = Vec::new();
     let _ = no_notes("You don't have any notes");
+    let _ = truncated_note(&mut options);
+    let selection = Select::new("Select the note that you want to view: ", options.clone()).prompt();
+    let selection_index = options.iter().position(|n| n == selection.as_ref().unwrap()).unwrap();
+    let selected_note = &saved_notes[selection_index];
 
-    for note in saved_notes {
-        println!("{}", note.name);
-        println!("{}", note.content);
-        println!("Created at: {}", note.created);
-        println!("=======================");
-    }
+    display_note(selected_note);
     return Ok(());
 }
 
@@ -104,83 +99,71 @@ fn edit_notes() -> Result<(), Box<dyn std::error::Error>> {
     let _ = truncated_note(&mut options);
     let selection = Select::new("Select the note that you want to edit: ", options.clone()).prompt();
     let selection_index = options.iter().position(|n| n == selection.as_ref().unwrap()).unwrap();
+    let selected_note = &saved_notes[selection_index];
+    let updated_note = Note {
+        id: selection_index,
+        name: Text::new("New name:").with_initial_value(&selected_note.name).prompt()?,
+        content: Text::new("New content:").with_initial_value(&selected_note.content).prompt()?,
+        created: selected_note.created.clone(),
+    };
 
-    match selection_index {
-        index => {
-            let selected_note = &saved_notes[index];
-            let edited_name = Text::new("New name:")
-                .with_initial_value(&selected_note.name)
-                .prompt()?;
-            let edited_content = Text::new("New content:")
-                .with_initial_value(&selected_note.content)
-                .prompt()?;
-
-            let updated_note = Note {
-                id: index,
-                name: edited_name,
-                content: edited_content,
-                created: selected_note.created.clone(),
-            };
-
-            let edit_note_bool = Confirm::new("Are you sure that you want to edit this note?")
-                .with_default(true)
-                .prompt();
+    let edit_note_bool = Confirm::new("Are you sure that you want to edit these notes?")
+        .with_default(true)
+        .prompt();
         
-            return match edit_note_bool {
-                Ok(true) => {
-                    api::edit_note(&updated_note, index)?;
-                    println!("Note updated successfully.");
-                    Ok(())
-                }
-                Ok(false) => Ok(()),
-                Err(e) => {
-                    println!("There was an error: {}", e);
-                    Err(Box::new(e))
-                }
-            };
+    return match edit_note_bool {
+        Ok(true) => {
+            api::edit_note(&updated_note, selection_index)?;
+            println!("Note updated successfully.");
+            Ok(())
         }
-    }
+        Ok(false) => Ok(()),
+        Err(e) => {
+            println!("There was an error: {}", e);
+            Err(Box::new(e))
+        }
+    };
 }
 
 fn delete_notes() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: Implement select multiple and delete all
+    // TODO: Implement select multiple
     let mut options: Vec<String> = Vec::new();
     let _ = no_notes("You can't delete notes, because there are none.");
     let _ = truncated_note(&mut options);
-    let selection = Select::new("Select the note that you want to delete: ", options.clone()).prompt();
-    let selection_index = options.iter().position(|n| n == selection.as_ref().unwrap()).unwrap();
+    let selections = MultiSelect::new("Select the notes that you want to delete: ", options.clone()).prompt();
+    let delete_note_bool = Confirm::new("Are you sure that you want to delete these notes?")
+        .with_default(true)
+        .prompt();
 
-    match selection_index {
-        index => {
-            let delete_note_bool = Confirm::new("Are you sure that you want to delete this note?")
-                .with_default(true)
-                .prompt();
-    
-            return match delete_note_bool {
-                Ok(true) => {
-                    api::delete_note(index)?;
-                    println!("Note deleted successfully.");
-                    Ok(())
-                }
-                Ok(false) => Ok(()),
-                Err(e) => {
-                    println!("There was an error: {}", e);
-                    Err(Box::new(e))
-                }
-            };
-        }
-    }
+    return Ok(for selection in selections.unwrap() {
+        let selection_index = options.iter().position(|n| n == &selection.as_ref()).unwrap(); 
+        
+        return match delete_note_bool {
+            Ok(true) => {
+                api::delete_note(selection_index)?;
+                println!("Notes deleted successfully.");
+                Ok(())
+            }
+            Ok(false) => Ok(()),
+            Err(e) => {
+                println!("There was an error: {}", e);
+                Err(Box::new(e))
+            }
+        };
+    });
+}
+
+fn display_note(note: &Note) {
+    println!("=======================");
+    println!("Name: {}", note.name);
+    println!("Content: {}", note.content);
+    println!("Created at: {}", note.created);
+    println!("=======================");
 }
 
 fn truncated_note(options: &mut Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
-    let saved_notes = api::get_notes()?;
-
-    Ok(for note in &saved_notes {
-        let mut truncated_content: String = note
-            .content
-            .chars()
-            .take(10)
-            .collect();
+    Ok(for note in &api::get_notes()? {
+        let mut truncated_content: String = note.content.chars().take(10).collect();
 
         if truncated_content.chars().count() == 10 {
             truncated_content = truncated_content + "...";
@@ -191,9 +174,7 @@ fn truncated_note(options: &mut Vec<String>) -> Result<(), Box<dyn std::error::E
 }
 
 fn no_notes(message: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let saved_notes = api::get_notes()?;
-
-    Ok(if saved_notes.is_empty() {
+    Ok(if api::get_notes()?.is_empty() {
         println!("{}", message);
         return Ok(());
     })
