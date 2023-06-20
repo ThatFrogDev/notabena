247e42b11b5a3767fa571f159ef9d64450372649
MrSerge01 <olgadeeva61@gmail.com>
2023-06-20 23:27:09 +0600

    inquire -> dialoguer
    Co-authored-by: Mart Zielman <ThatFrogDev@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 pub mod api;
 
 use chrono::prelude::*;
-use inquire::{Confirm, Select, MultiSelect, Text};
+use dialoguer::{theme::ColorfulTheme, Confirm, Select, MultiSelect, Input};
 
 pub struct Note {
     id: usize,
@@ -19,31 +19,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         cursor_to_origin();
 
         let options = vec!["New note", "View note", "Edit note", "Delete note", "Exit"];
-        let select = Select::new("What do you want to do?", options).prompt();
+        let select = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("What do you want to do?").items(&options).interact().unwrap();
 
         match select {
-            Ok("New note") => {
+            0 => {
                 new_note().expect("Creating a new note failed");
                 cursor_to_origin();
             },
-            Ok("View note") => {
+            1 => {
                 show_notes()?;
                 cursor_to_origin();
             },
-            Ok("Edit note") => {
+            2 => {
                 edit_notes().expect("Editing the note failed");
                 cursor_to_origin();
             },
-            Ok("Delete note") => {
+            3 => {
                 delete_notes().expect("Deleting the note failed");
                 cursor_to_origin();
             },
-            Ok(_) => {
+            _ => {
                 return Ok(());
-            },
-            Err(e) => {
-                println!("There was an error: {}", e);
-            },
+            }
         }
     }
 }
@@ -52,8 +50,8 @@ fn new_note() -> Result<(), Box<dyn std::error::Error>> {
     let saved_notes = api::get_notes()?;
     let inputted_note = Note {
         id: saved_notes.len(),
-        name: Text::new("Name:").prompt()?,
-        content: Text::new("Content:").prompt()?,
+        name: Input::with_theme(&ColorfulTheme::default()).with_prompt("Name:").interact_text().unwrap(),
+        content: Input::with_theme(&ColorfulTheme::default()).with_prompt("Content:").interact_text().unwrap(),
         created: format!("{}", Local::now().format("%A %e %B, %H:%M").to_string()),
     };
 
@@ -61,21 +59,16 @@ fn new_note() -> Result<(), Box<dyn std::error::Error>> {
     println!("This is the note you're about to create:");
     display_note(&inputted_note);
 
-    let save_note_bool = Confirm::new("Do you want to save this note?")
-        .with_default(true)
-        .prompt();
+    let save_note_bool = Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Do you want to save this note?").default(true).interact().unwrap();
 
     return match save_note_bool {
-        Ok(true) => {
+        true => {
             api::save_note(&inputted_note)?;
             println!("Note created successfully.");
             Ok(())
         }
-        Ok(false) => Ok(()),
-        Err(e) => {
-            println!("There was an error: {}", e);
-            Err(Box::new(e))
-        }
+        false => Ok(())
     };
 }
 
@@ -83,14 +76,17 @@ fn show_notes() -> Result<(), Box<dyn std::error::Error>> {
     let saved_notes = api::get_notes()?;
     let mut options: Vec<String> = Vec::new();
     truncated_note(&mut options)?;
-    let selection = Select::new("Select the note that you want to view:", options.clone()).prompt();
-    let selection_index = options.iter().position(|n| n == selection.as_ref().unwrap()).unwrap();
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select the note that you want to view:")
+        .items(&options)
+        .interact()
+        .unwrap();
 
     if api::get_notes()?.is_empty() {
         println!("{}", "You don't have any notes.");
         return Ok(());
     } else {
-        let selected_note = &saved_notes[selection_index];
+        let selected_note = &saved_notes[selection];
 
         display_note(selected_note);
         return Ok(());
@@ -101,36 +97,31 @@ fn edit_notes() -> Result<(), Box<dyn std::error::Error>> {
     let saved_notes = api::get_notes()?;
     let mut options: Vec<String> = Vec::new();
     truncated_note(&mut options)?;
-    let selection = Select::new("Select the note that you want to edit:", options.clone()).prompt();
-    let selection_index = options.iter().position(|n| n == selection.as_ref().unwrap()).unwrap();
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select the note that you want to edit:").items(&options).interact().unwrap();
 
     if api::get_notes()?.is_empty() {
         println!("{}", "You can't edit notes, because there are none.");
         return Ok(());
     } else {
-        let selected_note = &saved_notes[selection_index];
+        let selected_note = &saved_notes[selection];
         let updated_note = Note {
-            id: selection_index,
-            name: Text::new("New name:").with_initial_value(&selected_note.name).prompt()?,
-            content: Text::new("New content:").with_initial_value(&selected_note.content).prompt()?,
+            id: selection,
+            name: Input::with_theme(&ColorfulTheme::default()).with_prompt("New name:").interact_text().unwrap(),
+            content: Input::with_theme(&ColorfulTheme::default()).with_prompt("New content:").interact_text().unwrap(),
             created: selected_note.created.clone(),
         };
     
-        let edit_note_bool = Confirm::new("Are you sure that you want to edit this note?")
-            .with_default(true)
-            .prompt();
+        let edit_note_bool = Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("Are you sure that you want to edit this note?").default(true).interact().unwrap();
             
         return match edit_note_bool {
-            Ok(true) => {
-                api::edit_note(&updated_note, selection_index)?;
+            true => {
+                api::edit_note(&updated_note, selection)?;
                 println!("Note updated successfully.");
                 Ok(())
             }
-            Ok(false) => Ok(()),
-            Err(e) => {
-                println!("There was an error: {}", e);
-                Err(Box::new(e))
-            }
+            false => Ok(())
         };
     }
 }
@@ -139,29 +130,27 @@ fn delete_notes() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: Implement select multiple
     let mut options: Vec<String> = Vec::new();
     truncated_note(&mut options)?;
-    let selections = MultiSelect::new("Select the notes that you want to delete:", options.clone()).prompt();
-    let delete_note_bool = Confirm::new("Are you sure that you want to delete these notes?")
-        .with_default(true)
-        .prompt();
+    let selections = MultiSelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select the notes that you want to delete:").items(&options).interact().unwrap();
+
+    let delete_note_bool = Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Are you sure that you want to delete these notes?")
+        .default(true)
+        .interact()
+        .unwrap();
 
     if api::get_notes()?.is_empty() {
         println!("{}", "You can't delete notes, because there are none.");
         return Ok(());
     } else {
-        return Ok(for selection in selections.unwrap() {
-            let selection_index = options.iter().position(|n| n == &selection.as_ref()).unwrap(); 
-            
+        return Ok(for selection in selections {            
             return match delete_note_bool {
-                Ok(true) => {
-                    api::delete_note(selection_index)?;
+                true => {
+                    api::delete_note(selection)?;
                     println!("Notes deleted successfully.");
                     Ok(())
                 }
-                Ok(false) => Ok(()),
-                Err(e) => {
-                    println!("There was an error: {}", e);
-                    Err(Box::new(e))
-                }
+                false => Ok(())
             };
         });
     }
