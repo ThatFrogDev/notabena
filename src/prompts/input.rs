e863f6486224e38976da8c3f51c8188678569a07
ThatBoiDev <mart.zielman@kpnmail.nl>
2024-02-27 17:35:15 +0100

    feat: return key, chore: version increment

--- src/prompts/input.rs
+++ src/prompts/input.rs
@@ -1,7 +1,9 @@
+use crate::return_to_main::{continue_event, return_event};
+use crate::{main, return_to_main};
 use dialoguer::{theme::ColorfulTheme, Input};
 
-pub fn input(prompt: &str, initial_text: String) -> String {
-    match initial_text.as_str() {
+pub fn input(prompt: &str, initial_text: String) -> Result<String, Box<dyn std::error::Error>> {
+    let result = match initial_text.as_str() {
         "" => Input::with_theme(&ColorfulTheme::default())
             .with_prompt(prompt)
             .interact_text()
@@ -11,5 +13,18 @@ pub fn input(prompt: &str, initial_text: String) -> String {
             .with_initial_text(initial_text)
             .interact_text()
             .unwrap(),
+    };
+
+    match return_to_main() {
+        Ok(value) => {
+            if value == return_event() {
+                main()?;
+            } else if value == continue_event() {
+                return Ok(result);
+            }
+        }
+        _ => return Ok(result),
     }
+
+    Ok(result)
 }
